f12bcb73844b85bf65846ed2a23cd77dd0523a79
ArthurRamiro <114745651+ArthurRamiro@users.noreply.github.com>
2025-08-05 19:27:53 -0300

    Create AutenticacaoServico.java
    criei Autenticação Serviço

---
+++ src/br/com/techamv/servico/AutenticacaoServico.java
@@ -0,0 +1,14 @@
+package br.com.techamv.servico;
+
+import br.com.techamv.modelo.usuario.Usuario;
+import br.com.techamv.repositorio.BancoDeDados;
+import java.util.Optional;
+
+public class AutenticacaoServico {
+
+    public Optional<Usuario> fazerLogin(String email, String senha) {
+        return BancoDeDados.usuarios.stream()
+                .filter(u -> u.getEmail().equalsIgnoreCase(email) && u.getSenha().equals(senha))
+                .findFirst();
+    }
+}
