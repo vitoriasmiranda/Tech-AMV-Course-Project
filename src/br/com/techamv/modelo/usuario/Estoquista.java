e57576ecd96e1698abfa8e550e4103b69e3f0d8d
Vitória Miranda <vitoriamirandaa5@gmail.com>
2025-08-13 21:34:23 -0300

    .

--- src/br/com/techamv/modelo/usuario/Estoquista.java
+++ src/br/com/techamv/modelo/usuario/Estoquista.java
@@ -2,7 +2,7 @@ package br.com.techamv.modelo.usuario;
 
 public class Estoquista extends Usuario {
     public Estoquista(String nome, String email, String senha) {
-        super(nome, email, senha);
+        super(nome, email,);
     }
 
     @Override
